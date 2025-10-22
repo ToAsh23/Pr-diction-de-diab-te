10c9f85e25298711a456f6bd59ee74621ea063f9
ToAsh23 <165156220+ToAsh23@users.noreply.github.com>
2025-10-22 16:10:08 +0200

    Update diabete (1).py

--- diabete (1).py
+++ diabete (1).py
@@ -9,17 +9,9 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 import numpy as np
-from matplotlib.patches import Patch
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import RobustScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, accuracy_score, mean_squared_error, r2_score, roc_auc_score, roc_curve, classification_report
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn import metrics
-from sklearn import metrics
+
 
 
 st.set_page_config(page_title="Vision génerale", initial_sidebar_state='auto', layout="wide")
